19dcdd281d7379a450ee90dc77002867913f5f9d
Enayatullah Norozi <enayathushmand12@gmail.com>
2021-12-02 15:17:37 +0100

    better solution for day 1

--- day1/main.hs
+++ day1/main.hs
@@ -2,18 +2,15 @@
 import Data.Char
 
 countInc :: (Ord a, Num a) => [a] -> Int
-countInc inp = length $ filter (> 0) $ map (\(a,b) -> b-a) $ zip inp (drop 1 inp)
+countInc inp = length $ filter (>0) $ zipWith (\a b -> b-a) inp (tail inp)
 
 countTrippleInc :: (Ord a, Num a) => [a] -> Int
-countTrippleInc inp = length $ filter (>0) $ map (\(a,b) -> b-a) $ zip sums (drop 1 sums)
+countTrippleInc inp = countInc sums
             where
-                sums = map (\(a,b,c) -> a+b+c) $ zip3 inp (drop 1 inp) (drop 2 inp)
+                sums = zipWith3 (\a b c -> a+b+c) inp (drop 1 inp) (drop 2 inp)
 
 main = do
     inp <- readFile "input.txt"
     let arr = map (\x -> read x :: Integer) (lines inp) 
-    putStr "part 1: "
-    putStrLn $ show (countInc arr)
-    putStr "part 2: "
-    putStrLn $ show (countTrippleInc arr)
-
+    putStrLn $ "part 1: " ++ show (countInc arr)
+    putStrLn $ "part 2: " ++ show (countTrippleInc arr)
