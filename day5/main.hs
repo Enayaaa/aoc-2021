e8624ee195234ebf33d79a1a1e62861ec8679220
Enayatullah Norozi <enayathushmand12@gmail.com>
2021-12-05 14:35:05 +0100

    terrible but working solution thank you very much

--- day5/main.hs
+++ day5/main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE TupleSections #-}
-import           Data.Char       (isSpace)
-import           Data.Either     (partitionEithers)
-import           Data.List       (nub)
-import           Data.List.Split (dropBlanks, dropDelims, oneOf, split, splitOn)
-import           Data.Maybe      (fromJust, isJust)
-import           Test.QuickCheck (mapSize)
-import Debug.Trace
+import           Data.Char                 (isSpace)
+import           Data.Either               (partitionEithers)
+import           Data.List                 (nub)
+import           Data.List.Split           (dropBlanks, dropDelims, oneOf,
+                                            split, splitOn)
+import           Data.Maybe                (catMaybes, fromJust, isJust)
+import           Debug.Trace
+import           Distribution.Simple.Utils (withTempDirectory)
+import           Test.QuickCheck           (mapSize)
+import           Trace.Hpc.Mix             (BoxLabel)
 
 
 -- | splits, removes the delimeter and any other delimeter element
@@ -37,7 +40,7 @@ comb m xs = combsBySize xs !! m
 
 data Point = Point Int Int deriving (Show, Eq)
 data Line = Line Point Point deriving (Show, Eq)
-type Canvas = [[Maybe Int]]
+type Canvas = [[Int]]
 
 isStraitLine :: Line -> Bool
 isStraitLine (Line (Point x1 y1) (Point x2 y2)) = x1 == x2 || y1 == y2
@@ -78,11 +81,6 @@ passesThrough (Line (Point x1 y1) (Point x2 y2)) (Point x y) = isOnTheLine && is
 
 lineTrace :: Line -> [Point]
 lineTrace l
- --     = [  Point x y |
- --             x <- [min x1 x2..max x1 x2],
- --             y <- [min y1 y2..max y1 y2],
- --             passesThrough (Line (Point x1 y1) (Point x2 y2)) (Point x y)
- --       ]
     = zipWith Point (takeWhile (\x -> x <= max x1 x2 && x >= min x1 x2) xs) (takeWhile (\y -> y <= max y1 y2 && y >= min y1 y2) ys)
     where
         km = getLineEq (Line (Point x1 y1) (Point x2 y2))
@@ -90,7 +88,7 @@ lineTrace l
 
         (xs,ys) = case km of
             Just (k,m) -> ([(min x1 x2)+dx | dx <- [0..]], [k*x+m | x <- xs])
-            Nothing -> (repeat x1, [min y1 y2..])
+            Nothing    -> (repeat x1, [min y1 y2..])
 
 timesOverlaps :: Line -> Line -> Int
 timesOverlaps l1 l2 = sum $ map (\l -> count l (lineTrace l1)) (lineTrace l2)
@@ -105,26 +103,75 @@ getMaxCoords :: [Line] -> (Int, Int)
 getMaxCoords ls = (maximum xs, maximum ys)
     where (xs, ys) = unzip . map (\(Line (Point x1 y1) (Point x2 y2)) -> (max x1 x2,max y1 y2)) $ ls
 
--- markCanvasIndex c (Point x y) = foo 0 0 x y c
---     where foo a b x y (c:cs)
---             | a == x && b == y = if isJust c
---                                     then
+markCanvasIndex c (Point x y) = insert2D (count+1) y x c
+    where count = (c !! y) !! x
 
--- paint :: Canvas -> Line -> Canvas
--- paint c l = map (\(Point x y) -> (c !! x) !! y =) coords
---     where coords = lineTrace l
+-- Either replace the old first element with the new first element,
+-- or insert the new element into the tail of the list and prepend
+-- the old first element.
+insert1D :: a -> Int -> [a] -> [a]
+insert1D x' 0 (_:xs) = x':xs
+insert1D x' p (x:xs) = x : insert1D x' (p - 1) xs
+
+-- Likewise, either replace the old first row with a new first row
+-- (which you get using insert1D), or insert the new element into
+-- the tail of the array and prepend the old first row.
+insert2D :: a -> Int -> Int -> [[a]] -> [[a]]
+insert2D x'  0 py (r:rs) = insert1D x' py r : rs
+insert2D x' px py (r:rs) = r : insert2D x' (px - 1) py rs
+
+
+{-
+[
+[0,0,0,0,0,0,0,1,0,0],
+[0,0,1,0,0,0,0,1,0,0],
+[0,0,1,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,0,0],
+[0,1,1,1,1,1,1,1,1,2],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[1,1,1,1,1,1,0,0,0,0]
+]
+
+[
+[0,0,0,0,0,0,0,1,0,0],
+[0,0,1,0,0,0,0,1,0,0],
+[0,0,1,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,0,0],
+[0,1,1,2,1,1,1,2,1,1],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[2,2,2,1,1,1,0,0,0,0]
+]
+
+-}
+
+isDiagonalLine :: Line -> Bool
+isDiagonalLine (Line (Point x1 y1) (Point x2 y2)) = abs (x2-x1) == abs (y2 - y1)
+
+paint :: Canvas -> Line -> Canvas
+paint c l = foo c {-trace (show l ++ " -> "++show (lineTrace l)) -} (lineTrace l)
+    where foo c []     = c
+          foo c (x:xs) = foo (markCanvasIndex c x) xs
 
 --solve1 :: [Line] -> Int
--- solve1 ls = foo (replicate xmax (replicate ymax Nothing)) ls
---     where
---         (xmax, ymax) = getMaxCoords ls
---         foo :: Canvas -> [Line] -> Canvas
---         foo c []     = c
---         foo c (l:ls) = foo (paint c l) ls
+solve1 ls = length . filter (>1) . concat . take (xmax+1) . map (take (ymax+1)) $ foo (repeat (repeat 0)) ls
+    where
+        (xmax, ymax) = getMaxCoords ls
+        foo :: Canvas -> [Line] -> Canvas
+        foo c []     = c
+        foo c (l:ls) = foo (paint c l) ls
 
-solve1 = sum . map ( uncurry timesOverlaps) . pairs
+--solve1 = sum . map ( uncurry timesOverlaps) . pairs
+
+solve2 = solve1
 
 main :: IO ()
 main = do
     file <- readFile "input.txt"
     putStrLn $ "Part 1:"++(show . solve1 . filter isStraitLine . prepare $ file)
+    putStrLn $ "Part 2:"++(show . solve2 . filter (\l -> isStraitLine l || isDiagonalLine l) . prepare $ file)
