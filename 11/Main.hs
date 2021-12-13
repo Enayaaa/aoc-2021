cbcc0591f7d4ec7d883a3163c2591fd73f505471
Enayatullah Norozi <enayathushmand12@gmail.com>
2021-12-13 09:46:42 +0100

    cleanup

--- 11/Main.hs
+++ 11/Main.hs
@@ -2,38 +2,42 @@ module Main where
 
 -- Using priority queue for some reason, is not needed, better to use Map,
 -- I thaught it would be needed but the priority queue is not even used as it
--- is supposed to.
+-- is supposed to. But can't be bothered to change it for now.
+-- Ugliest code I have written so fat :)
 
 import           Data.Char            (digitToInt)
 import           Data.List            (sortBy)
 import qualified Data.PQueue.Prio.Max as P
 import qualified Data.Set             as Set
 
+type Point = (Int,Int)
+type EnergyQueue = P.MaxPQueue Int Point
+
 prepare :: String -> [[Int]]
 prepare = map (map digitToInt) . lines
 
-isValidPoint :: [[Int]] -> (Int, Int) -> Bool
+isValidPoint :: [[Int]] -> Point -> Bool
 isValidPoint xs (x,y)
     =  x >= 0 && x < length xs
     && y >= 0 && y < length (head xs)
 
-neighbours :: [[Int]] -> (Int, Int) -> [(Int, Int)]
+neighbours :: [[Int]] -> Point -> [Point]
 neighbours xs (x,y)
     = filter (isValidPoint xs)
     [(x+1,y),(x+1,y+1),(x+1,y-1),(x,y+1),(x,y-1),(x-1,y+1),(x-1,y),(x-1,y-1)]
 
-getPoint :: [[a]] -> (Int, Int) -> a
+getPoint :: [[a]] -> Point -> a
 getPoint xs (x,y) = xs !! x !! y
 
-willFlash :: [[Int]] -> (Int, Int) -> Bool
+willFlash :: [[Int]] -> Point -> Bool
 willFlash xs p = getPoint xs p == 9
 
-points :: [[a]] -> [(Int, Int)]
+points :: [[a]] -> [Point]
 points xs = [(x,y) | x <- [0..width], y <- [0..height]]
     where width = length (head xs) - 1
           height = length xs - 1
 
-neighs :: [[Int]] -> (Int, (Int, Int)) -> [(Int, (Int, Int))]
+neighs :: [[Int]] -> (Int, Point) -> [(Int, Point)]
 neighs xs (k,v) = zip ps ns
     where ns = neighbours xs v
           ps = map (getPoint xs) ns
@@ -41,7 +45,7 @@ neighs xs (k,v) = zip ps ns
 count :: Eq a => a -> [a] -> Int
 count e = length . filter (==e)
 
-flash :: [[Int]] -> P.MaxPQueue Int (Int,Int) -> Set.Set (Int, Int) -> (P.MaxPQueue Int (Int,Int), Set.Set (Int,Int))
+flash :: [[Int]] -> EnergyQueue -> Set.Set Point -> (EnergyQueue, Set.Set Point)
 flash xs pq flashed
     | null flashing = (incs, allFlashed)
     | otherwise     = flash (pqToMatrix incs) incs allFlashed
@@ -55,16 +59,16 @@ flash xs pq flashed
                  . filter (\(k,v) -> v `Set.notMember` flashed)
                  $ toFlash pq
 
-step :: P.MaxPQueue Int (Int, Int) -> (Int, P.MaxPQueue Int (Int, Int))
+step :: EnergyQueue -> (Int, EnergyQueue)
 step pq = (length flashed, P.foldlWithKey (\q k v -> if Set.member v flashed then P.insert 0 v q else P.insert k v q) P.empty result)
     where
         incs = P.mapKeys (+1) pq
         (result, flashed) = flash (pqToMatrix pq) incs Set.empty
 
-toFlash :: P.MaxPQueue Int (Int,Int) -> [(Int, (Int,Int))]
+toFlash :: EnergyQueue -> [(Int, Point)]
 toFlash = P.takeWhileWithKey (\k x -> k>9)
 
-counting :: Int -> Int -> P.MaxPQueue Int (Int, Int) -> Int
+counting :: Int -> Int -> EnergyQueue -> Int
 counting 0 s _ = s
 counting n s p = counting (n-1) (s+c) newp
     where (c,newp) = step p
@@ -75,7 +79,7 @@ solve1 xs
     . P.fromList
     $ zip (concat xs) (points xs)
 
-pqToMatrix :: P.MaxPQueue Int (Int,Int) -> [[Int]]
+pqToMatrix :: EnergyQueue -> [[Int]]
 pqToMatrix
     = takeBy 10
     . map snd
@@ -87,7 +91,7 @@ takeBy :: Int -> [a] -> [[a]]
 takeBy _ [] = []
 takeBy n ls = take n ls : takeBy n (drop n ls)
 
-foo :: Int -> P.MaxPQueue Int (Int, Int) -> Int
+foo :: Int -> EnergyQueue -> Int
 foo n p
     | P.size (P.dropWhileWithKey (\k v -> k==0) newp) == 0 = n
     | otherwise = foo (n+1) newp
--- 12/main.hs
+++ 12/main.hs
@@ -1,8 +1,6 @@
 import           Data.Char       (isUpper)
 import           Data.List       (group, sort)
 import           Data.List.Split (splitOn)
-import           Data.Maybe      (isJust, isNothing)
-import           Debug.Trace     (trace)
 
 
 data Cave = Start | End | BigCave String | SmallCave String deriving (Eq, Ord)
@@ -47,7 +45,8 @@ dfs' _     visited End  = 1
 dfs' edges visited cave
     | isSmall cave || cave == Start = sum $ map (dfs' edges (cave:visited) . end) toTraverse
     | otherwise = sum $ map (dfs' edges visited . end) toTraverse
-    where toTraverse = filter (\edge -> end edge `notElem` visited) $ neighbors cave edges
+    where
+        toTraverse = filter (\edge -> end edge `notElem` visited) $ neighbors cave edges
 
 solve1 :: [Edge] -> Int
 solve1 edges = dfs (edges++map (\(Edge s e) -> Edge e s) edges)
@@ -75,22 +74,11 @@ smallCaveTwiceOccurences :: [Cave] -> [[Cave]]
 smallCaveTwiceOccurences
     = filter ((>1) . length) . group . sort . filter isSmall
 
--- dfs2 :: [Edge] -> Int
--- dfs2 edges = dfs2' edges Nothing [] Start
-
--- dfs2' :: [Edge] -> Maybe Cave -> [Cave] -> Cave ->Int
--- dfs2' _     _     visited End  = trace (show $ reverse (End:visited)) 1
-
--- dfs2' edges twice visited cave
---     | isSmall cave = sum $ map (dfs2' edges (if isNothing twice && cave `elem` visited then Just cave else twice) (cave:visited) . end) toTraverse
---     | cave == Start = sum $ map (dfs2' edges twice visited . end) toTraverse
---     | otherwise = sum $ map (dfs2' edges twice (cave:visited) . end) toTraverse
---     where toTraverse = filter (\edge -> if isJust twice || end edge == Start then end edge `notElem` visited else  count (end edge) visited < 2) $ neighbors cave edges
-
+dfs2 :: [Edge] -> Int
 dfs2 edges = dfs2' edges [] Start
 
 dfs2' :: [Edge] -> [Cave] -> Cave ->Int
-dfs2' _     visited End  = trace (show $ reverse (End:visited)) $ if length (smallCaveTwiceOccurences visited) > 1 then 0 else 1
+dfs2' _     visited End  = if length (smallCaveTwiceOccurences visited) > 1 then 0 else 1
 dfs2' edges visited cave = sum $ map (dfs2' edges (cave:visited) . end) toTraverse
     where toTraverse = filter (canTraverse' visited) $ neighbors cave edges
 
